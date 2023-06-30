5ec23c8cc576363692fcc202e3755e7e9a7d6a21
cintia89 <cintia.silvadeooliveira@gmail.com>
2023-06-30 15:36:17 -0300

    feat(src):criacao da coletion parceires e eventos @cintia89 @nicofercavv-dev

---
+++ src/api/parceire/controllers/parceire.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * parceire controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::parceire.parceire');
