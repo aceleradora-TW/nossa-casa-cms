fa7f7c36eb11aa7c14ec5807bbef0824f645e506
cintia89 <cintia.silvadeooliveira@gmail.com>
2023-07-07 10:28:08 -0300

    refactor:(colecão parceires): adicionar campo de tipo na colecão de parceires - @cintia89 @pabloHunter

--- src/api/workshop/controllers/workshop.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * workshop controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::workshop.workshop');
